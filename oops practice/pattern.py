07e8a0d3ab4dc95f24eaad147d6686de0ddbe683
prink-patel <155448064+prink-patel@users.noreply.github.com>
2024-01-05 11:39:30 +0530

    pattern.py

--- oops practice/pattern.py
+++ oops practice/pattern.py
@@ -1,43 +1,49 @@
 
 
-n=5
+n=11
 
 c=1
 a=(n*2)+3
+
 
 for i in range(a):
     b=n+1
     b=b-i+1
     c=c+i
     z=1
+    x=2
         
     p=1
     for j in range(a):
 
         if j==0 or j==(a-1) or (i==(a//2) and j==(a//2)):
-            print("o",end="")
+            print(" o",end=" ")
 
         elif i==0 or i==(a-1) :
-            print("0",end="") 
+            
+            print(" 0",end=" ") 
 
         elif i<=j and j<n+1:
+            
             b=b-1
-            print(b,end="")
+            print(f"{b:2}",end=" ")
+            
 
         elif j==a-c and i<n+1:
-            print(c-1,end="")
+            print(f"{c-1:2}",end=" ")
             c=c-1
 
         elif  i>a//2 and j<a-i:
-            print(z,end="")
+            print(f"{z:2}",end=" ")
             z=z+1
 
         elif (i<=j and j<a//2) or (i>=j and j>a//2):
             
-            print(p,end="")
+            print(f"{p:2}",end=" ")
             p=p+1
         
-        else:
-            print(" ",end="")
+        else: 
+            print("  ",end=" ")
+       
     
     print()
