41763ab4d78b866ce4e8d8236b098cec48e84101
prink-patel <prink.hapaliya@weboccult.com>
2024-03-28 16:20:12 +0530

    delet majot Review Task

--- OpenCV/Major Review Task:/demo.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('best.pt')
-
-results = model.predict('rtsp://test:test@1234@192.168.44.245:554/cam/realmonitor?channel=2&subtype=0', imgsz=920, show=True, conf=0.4, save=True)
