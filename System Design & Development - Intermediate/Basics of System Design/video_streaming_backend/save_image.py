351dd71be59159c7ce0fa3e36dfc7e64430fff01
prink-patel <prink.hapaliya@weboccult.com>
2024-05-03 17:13:53 +0530

    add comment

--- System Design & Development - Intermediate/Basics of System Design/video_streaming_backend/save_image.py
+++ System Design & Development - Intermediate/Basics of System Design/video_streaming_backend/save_image.py
@@ -12,6 +12,7 @@ class SaveImage:
         logger.info("Image saved object create")
         self.count = 0
 
+    #decode image from base64
     def run(self, img_str):
         try:
             logger.info("Image saved run method started")
@@ -23,6 +24,7 @@ class SaveImage:
         except Exception as e:
             logger.critical("image not valid formate or not found")
 
+    # save image
     def save_image(self):
         logger.info("Image saved")
         img_path = os.path.join(r"demo", r"images", f"{self.count}_{str(uuid.uuid1())}.jpg")
